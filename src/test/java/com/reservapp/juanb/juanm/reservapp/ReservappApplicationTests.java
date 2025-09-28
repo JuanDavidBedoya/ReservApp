c3f4ef1b4da1f2d47e97e96fe2476d426e20efac
JuanDavidBedoya <bedoyaj919@gmail.com>
2025-09-27 21:15:41 -0500

    creación del repositorio

---
+++ src/test/java/com/reservapp/juanb/juanm/reservapp/ReservappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservapp.juanb.juanm.reservapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
