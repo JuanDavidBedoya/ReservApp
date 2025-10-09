37408c9bcf5f3bfe3623f4915eb98007f99b078c
JuanMartinezB <juans.martinezb@uqvirtual.edu.co>
2025-10-08 23:51:03 -0500

    Tailwind actualizado para rutas

--- TailApp/src/app/app.routes.ts
+++ TailApp/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { InicioSesion } from './pages/inicio-sesion/inicio-sesion';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:Home},
+    {path: 'inicio-sesion', component:InicioSesion},
+    {path: '**', redirectTo: ''}
+];
